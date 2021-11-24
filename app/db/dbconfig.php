7c7c6bb6edf1eb4882907403b006b8335c78dc35
B-student <Bingoleley2015@gmail.com>
2021-11-24 10:08:05 +0100

    g

---
+++ app/db/dbconfig.php
@@ -0,0 +1,5 @@
+<?php
+$serverName = "mysql";
+$username = "root";
+$password="secret123";
+$databasename="Guestbook";
--- app/public/logIn.php
+++ app/public/logIn.php
@@ -4,14 +4,41 @@ $_userName = $_POST["username"];
 $_password = $_POST["password"];
 
 if ($_userName=="Biniam" && $_password=="Biniam123") {
-    echo "Welcome Biniam";
+    echo "Welcome Biniam\n";
 }
 elseif ($_userName=="Amrish" && $_password=="Amrish123") {
-    echo "Welcome Amrish";
+    echo "Welcome Amrish\n";
 }
 elseif ($_userName=="Musavir" && $_password=="Musavir123") {
-    echo "Welcome Musavir";
+    echo "Welcome Musavir\n";
 }
 else {
-    echo"Account dont exist";
+    echo"Account dont exist\n";
+}
+
+require_once("/app/db/dbconfig.php");
+try {
+    $connection = new PDO("mysql:host=$serverName;dbname=$databasename",$username,$password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+   
+    echo"<br> connected successfully";
+} catch (PDOException $e) {
+    echo"<br>";
+    echo"Connection failed: ".$e->getmessage();
+}
+
+$sql = "SELECT * FROM `posts`";
+
+foreach($connection->query($sql) as $row){
+
+    echo $row["id"]."<br>";
+
+    echo $row["posted_at"]."<br>";
+
+    echo $row["name"]."<br>";
+
+    echo $row["email"]."<br>";
+
+    echo $row["message"]."<br>";
+
 }
